3f407ced67a1fd4fbdd6c7360aac8f53756af82c
next1code <next1code@yahoo.com>
2026-07-30 22:45:26 +0430

    feat: refactor DarkMode component to use clsx for conditional class names and improve class management

--- src/ui/DarkMode.tsx
+++ src/ui/DarkMode.tsx
@@ -1,6 +1,6 @@
 import { useTheme } from "../context/ThemeContext";
-import Button from "./Button";
 import { Sun, Moon } from "lucide-react";
+import clsx from "clsx";
 
 function DarkMode() {
   const { theme, toggleTheme } = useTheme();
@@ -8,30 +8,33 @@ function DarkMode() {
   return (
     <button
       onClick={toggleTheme}
-      className={`relative w-18 h-8 rounded-full outline-none ${
+      className={clsx(
+        "relative w-18 h-8 rounded-full outline-none transition-colors duration-500 overflow-hidden",
         theme === "light"
           ? "bg-gray-200 shadow-[inset_-3px_1px_10px_rgba(3,3,3,0.4)] "
-          : "bg-yellow-50 shadow-[inset_6px_2px_10px_rgba(3,3,3,0.6)]"
-      } transition-colors duration-500 overflow-hidden`}
+          : "bg-yellow-50 shadow-[inset_6px_2px_10px_rgba(3,3,3,0.6)]",
+      )}
     >
       <span
-        className={`absolute left-2  top-1/2 -translate-y-1/2 ${
-          theme === "dark" && "translate-x-8"
-        } transition-all duration-300 `}
+        className={clsx(
+          "absolute left-2  top-1/2 -translate-y-1/2 transition-transform duration-300",
+          theme === "dark" ? "translate-x-8" : "translate-x-0",
+        )}
       >
         <Sun
-          color="#f69e0a"
           size={22}
-          className={`absolute transition-all duration-300 ${
-            theme === "light" ? "opacity-0" : "opacity-100"
-          }`}
+          className={clsx(
+            "absolute text-[#f69e0a] transition-opacity duration-300",
+            theme === "light" ? "opacity-0" : "opacity-100",
+          )}
         />
         <Moon
           color="#333333"
           size={22}
-          className={`transition-all duration-300 ${
-            theme === "light" ? "opacity-100" : "opacity-0"
-          }`}
+          className={clsx(
+            "text-[#333333] transition-opacity duration-300",
+            theme === "light" ? "opacity-100" : "opacity-0",
+          )}
         />
       </span>
     </button>
